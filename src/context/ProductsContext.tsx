7e64a8a4bbc44087ebbb5f86eab71225d8a03971
Adrian Roger <adrianroge@gmail.com>
2024-07-17 12:33:07 -0300

    fix: change useeffect to usememo to prevent unnecessary re rendering

--- src/context/ProductsContext.tsx
+++ src/context/ProductsContext.tsx
@@ -1,19 +1,19 @@
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 import { IItem } from '../interfaces/interfaces';
 import { faker } from '@faker-js/faker';
 
 interface ProductsContextProps {
   products: IItem[];
-  fetchProducts: () => void;
+  // fetchProducts: () => void;
 }
 
-export const ProductsContext = createContext<ProductsContextProps>({products:[], fetchProducts: () => {}});
+export const ProductsContext = createContext<ProductsContextProps>({products:[]});
 
 export const ProductsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [products, setProducts] = useState<IItem[]>([]);
 
-  const fetchProducts = () => {
-    const fakeProducts:IItem[] = Array.from({length: 50}, (_, id) => ({
+  React.useMemo(() => {
+    const fakeProducts:IItem[] = Array.from({length: 500}, (_, id) => ({
       id,
       name: faker.commerce.productName(),
       description: faker.commerce.productDescription(),
@@ -23,14 +23,10 @@ export const ProductsProvider: React.FC<{ children: ReactNode }> = ({ children }
     }))
 
     setProducts(fakeProducts)
-  };
-
-  useEffect(() => {
-    fetchProducts();
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ products, fetchProducts }}>
+    <ProductsContext.Provider value={{ products }}>
       {children}
     </ProductsContext.Provider>
   );
